240eef01940346960b17270420fdc55002755c43
Zedd <zeddsnow@gmail.com>
2022-12-21 09:36:50 +0000

    allow user to open and close login form

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-document.getElementById('man');
+const loginNav = document.getElementById('login-nav');
+const loginOpen = document.getElementById('loginopen');
+const closelog = document.querySelectorAll('.fa-close');
+
+// displays login form
+loginNav.addEventListener('click', () => {
+  loginOpen.style.display = 'flex';
+});
+
+// closes login form
+closelog.forEach((logop) => logop.addEventListener('click', () => {
+  loginOpen.style.display = 'none';
+}));
